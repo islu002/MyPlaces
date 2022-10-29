b7aa12b2dee53f471057b77d8afe2c8bd09a88c2
Luis Labarta <islu_002@hotmail.com>
2022-10-29 20:31:11 +0200

    PAC2-Ex1

--- MyPlaces/DetailController.swift
+++ MyPlaces/DetailController.swift
@@ -9,57 +9,57 @@ import UIKit
 
 class DetailController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
    
-    let types = ["Generic Place", "Tourist Place"]
+    // Places types
+    let pickerElems1 = ["Generic Place", "Tourist Place"]
+    
     var manager = ManagerPlaces.shared()
     var index: Int = 0
 
-    @IBOutlet weak var name: UITextField!
-
-    @IBOutlet weak var image: UIImageView!
-    @IBOutlet weak var chooseType: UIPickerView!
-
-    @IBOutlet weak var notes: UITextView!
+    @IBOutlet weak var viewPicker: UIPickerView!
+    @IBOutlet weak var imagePicked: UIImageView!
+    @IBOutlet weak var textName: UITextField!
+    @IBOutlet weak var textDescription: UITextView!
     
-    var place: Place!
+    var place: Place? = nil
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return types.count
+        return pickerElems1.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return types[row]
+        return pickerElems1[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if (row == 0) {
-            place.type = PlacesTypes.GenericPlace
+            place?.type = PlacesTypes.GenericPlace
         }
         else {
-            place.type = PlacesTypes.TouristPlace
+            place?.type = PlacesTypes.TouristPlace
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        name.text = place.name
-        notes.text = place.description
-        image.image = UIImage(named: "sun.png")
+        textName.text = place?.name
+        textDescription.text = place?.description
+        imagePicked.image = UIImage(named: "sun.png")
 
         // Do any additional setup after loading the view.
         
         //self.constrainHeight.constant = 400
-        chooseType.delegate = self
-        chooseType.dataSource = self
-        index = manager.GetPosition(place)
+        viewPicker.delegate = self
+        viewPicker.dataSource = self
+        index = manager.GetPosition(place!)
     }
   
     override func viewDidAppear(_ animated: Bool) {
-        if (place.type == PlacesTypes.TouristPlace) {
-            chooseType.selectRow(1, inComponent: 0, animated: false)
+        if (place?.type == PlacesTypes.TouristPlace) {
+            viewPicker.selectRow(1, inComponent: 0, animated: false)
         }
     }
     
@@ -80,16 +80,16 @@ class DetailController: UIViewController, UIPickerViewDelegate, UIPickerViewData
     */
 
     @IBAction func deletePlace(_ sender: Any) {
-        manager.remove(place)
+        manager.remove(place!)
         dismiss(animated: true, completion: nil)
     }
     
 
     @IBAction func updatePlace(_ sender: Any) {
-        if (name.text!.isEmpty == false && notes.text.isEmpty == false) {
-            place.name = name.text!
-            place.description = notes.text
-            manager.update(place, index: index)
+        if (textName.text!.isEmpty == false && textDescription.text.isEmpty == false) {
+            place?.name = textName.text!
+            place?.description = textDescription.text
+            manager.update(place!, index: index)
             dismiss(animated: true, completion: nil)
         }
         else {
@@ -97,5 +97,8 @@ class DetailController: UIViewController, UIPickerViewDelegate, UIPickerViewData
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
+    }
+    @IBAction func selectImage(_ sender: Any) {
+        
     }
 }
